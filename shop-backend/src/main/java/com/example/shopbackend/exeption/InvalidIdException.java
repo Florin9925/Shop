794e47c1ea9fc847345c0026d3b8f55b6a733061
Florin Arhip <florin9925@gmail.com>
2022-04-06 23:55:46 +0300

    Added custom exceptions

---
+++ shop-backend/src/main/java/com/example/shopbackend/exeption/InvalidIdException.java
@@ -0,0 +1,24 @@
+package com.example.shopbackend.exeption;
+
+public class InvalidIdException extends RuntimeException{
+    public InvalidIdException() {
+        super();
+    }
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+
+    public InvalidIdException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidIdException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InvalidIdException(String message, Throwable cause, boolean enableSuppression,
+                                   boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
